2c202216e321982cf46337d55c1e52785ad89d85
Adrian Sepulveda <89766360+AdrianSep28@users.noreply.github.com>
2024-04-15 22:43:14 +0800

    a

--- Imps/src/main/java/com/Capstone/Imps/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.energywise.imps.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.energywise.imps.Entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-    UserEntity findByUsername(String username);
-}
