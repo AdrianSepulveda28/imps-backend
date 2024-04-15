7136fcbe800b410cd83dcd24e7ac634997f657c2
Adrian Sepulveda <89766360+AdrianSep28@users.noreply.github.com>
2024-04-15 21:10:36 +0800

    a

---
+++ IMPS/src/test/java/Capstone/example/IMPS/ImpsApplicationTests.java
@@ -0,0 +1,13 @@
+package Capstone.example.IMPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
