8dd98bbdf9ac36cddb6ca689664acec55c9e6156
Adrian Sepulveda <89766360+AdrianSep28@users.noreply.github.com>
2024-04-15 21:47:56 +0800

    a

--- Imps/src/main/java/com/Capstone/Imps/Entity/UserEntity.java
+++ Imps/src/main/java/com/Capstone/Imps/Entity/UserEntity.java
@@ -1,3 +1,110 @@
+package com.energywise.imps.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.energywise.imps.ImageUtility;
+
+@Entity
+@Table(name = "tbluser")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int user_id;
+
+    @Column(name = "user_username")
+    private String username;
+
+    @Column(name = "user_firstname")
+    private String firstname;
+
+    @Column(name = "user_lastname")
+    private String lastname;
+
+    @Column(name = "user_password")
+    private String password;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "user_isDeleted")
+    private boolean isDeleted;
+
+    public UserEntity() {
+        super();
+    }
+
+    public UserEntity(int user_id, String username, String firstname, String lastname, String password, String email,
+            byte[] picture, boolean isDeleted) {
+        this.user_id = user_id;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+        this.email = email;
+        this.isDeleted = false;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
 
 }
