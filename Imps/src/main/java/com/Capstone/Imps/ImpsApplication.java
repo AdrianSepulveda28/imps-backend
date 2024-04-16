792ec7ff4868fb118e638899de011330021ceb93
Adrian Sepulveda <89766360+AdrianSep28@users.noreply.github.com>
2024-04-16 18:38:29 +0800

    a

--- Imps/src/main/java/com/Capstone/Imps/ImpsApplication.java
+++ Imps/src/main/java/com/Capstone/Imps/ImpsApplication.java
@@ -1,13 +1,13 @@
-package com.Capstone.Imps;
+package com.Capstone.imps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImpsApplication {
+public class impsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImpsApplication.class, args);
+		SpringApplication.run(impsApplication.class, args);
 	}
 
 }
