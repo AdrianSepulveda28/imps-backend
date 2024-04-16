ce68de8ecef049da9aad69f3c53c8c62f902ad7b
Adrian Sepulveda <89766360+AdrianSep28@users.noreply.github.com>
2024-04-16 18:31:22 +0800

    a

--- Imps/src/main/java/com/Capstone/Imps/Service/UserRegistrationService.java
+++ Imps/src/main/java/com/Capstone/Imps/Service/UserRegistrationService.java
@@ -7,24 +7,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RegistrationService {
+public class UserRegistrationService {
 
-    private final RegistrationRepository registrationRepository;
+    private final UserRegistrationRepository userregistrationRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(RegistrationRepository registrationRepository, PasswordEncoder passwordEncoder) {
+    public RegistrationService(UserRegistrationRepository registrationRepository, PasswordEncoder passwordEncoder) {
         this.registrationRepository = registrationRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public RegistrationEntity registerUser(String username, String password, String email) {
+    public UserRegistrationEntity registerUser(String username, String password, String email) {
 
         String encodedPassword = passwordEncoder.encode(password);
 
-        RegistrationEntity newUser = new RegistrationEntity(username, encodedPassword, email);
+        UserRegistrationEntity newUser = new UserRegistrationEntity(username, encodedPassword, email);
 
-        return registrationRepository.save(newUser);
+        return userregistrationRepository.save(newUser);
     }
 
 }
