87cb909b746c34ea9acaca9068101851a320c34b
Adrian Sepulveda <89766360+AdrianSep28@users.noreply.github.com>
2024-04-16 18:35:06 +0800

    a

--- Imps/src/main/java/com/Capstone/Imps/Controller/UserRegistrationController.java
+++ Imps/src/main/java/com/Capstone/Imps/Controller/UserRegistrationController.java
@@ -1,34 +1,37 @@
 package com.imps.imps.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.imps.imps.Entity.RegistrationEntity;
+import com.imps.imps.Service.RegistrationService;
 
 @CrossOrigin(origins = "http://localhost:3000")
-public class RegistrationController {
-    private String username;
-    private String password;
-    private String email;
+@RestController
+@RequestMapping("/user")
+public class UserRegistrationController {
 
-    public String getUsername() {
-        return username;
-    }
+    @Autowired
+    private RegistrationService registrationService;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody UserRegistrationDto registrationDto) {
+        RegistrationEntity newUser = new RegistrationEntity();
+        newUser.setUsername(registrationDto.getUsername());
+        newUser.setPassword(registrationDto.getPassword());
+        newUser.setEmail(registrationDto.getEmail());
 
-    public String getPassword() {
-        return password;
-    }
+        RegistrationEntity savedUser = registrationService.registerUser(newUser);
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+        if (savedUser != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to register user.");
+        }
     }
 }
